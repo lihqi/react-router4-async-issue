c9dfc27109e4dd14d7c0bcb8f470bde78617fe19
lihqi <455711093@qq.com>
2018-12-21 10:12:16 +0800

    添加build

---
+++ build/precache-manifest.2256bb7da3e3afd631f7bfe4ad5def69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6586ea9dd55b755b2c8a",
+    "url": "/static/js/runtime~main.6586ea9d.js"
+  },
+  {
+    "revision": "1216dc6b7887b8b8c4da",
+    "url": "/static/js/main.1216dc6b.chunk.js"
+  },
+  {
+    "revision": "324f67db19d3a76ba438",
+    "url": "/static/js/Home.324f67db.chunk.js"
+  },
+  {
+    "revision": "98f342384e9ce3eac732",
+    "url": "/static/js/2.98f34238.chunk.js"
+  },
+  {
+    "revision": "1216dc6b7887b8b8c4da",
+    "url": "/static/css/main.b3e90aa1.chunk.css"
+  },
+  {
+    "revision": "6bf9672da723f1e5f28e5e5c8843ef59",
+    "url": "/index.html"
+  }
+];
